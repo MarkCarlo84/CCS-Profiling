1110093246d639674dd837dc2ea7e29bd58c3bde
Carlo <markcarlo0926@gmail.com>
2026-04-05 11:49:04 +0800

    fixed legacy

---
+++ backend/database/migrations/2026_04_05_000001_remove_legacy_bsit_bscs_subjects.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::table('subjects')->whereIn('program', ['BSIT', 'BSCS'])->delete();
+    }
+
+    public function down(): void {}
+};
